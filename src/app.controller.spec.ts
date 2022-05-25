a2bd1fe4da68e4eea9da084a9d54339523769ef7
Gabriele Partiti <gabrielepartiti@icloud.com>
2022-05-26 01:33:18 +0200

    add api controller

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
+import { CreationModule } from './modules/creation/creation.module';
+import { ValidationModule } from './modules/validation/validation.module';
+import { CaptchaNotPresentStatus } from './exceptions/CaptchaNotPresent';
+import { RedisModule } from './modules/redis/redis.module';
+import { RedisService } from './modules/redis/redis.service';
+import { CaptchaWrongAttemptStatus } from './exceptions/CaptchaWrongAttempt';
 
 describe('AppController', () => {
   let appController: AppController;
+  let redisService: RedisService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot({
+          load: [configuration],
+          isGlobal: true,
+        }),
+        CreationModule,
+        ValidationModule,
+        RedisModule,
+      ],
       controllers: [AppController],
-      providers: [AppService],
+      providers: [RedisService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    redisService = app.get<RedisService>(RedisService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('chapta', () => {
+    it('Should create new chapta', async () => {
+      const response = await appController.createChapta();
+
+      expect(response.uuid).toBeDefined();
+      expect(response.captcha).toBeDefined();
+    });
+
+    it('Should return error from validation', async () => {
+      try {
+        await appController.validateChapta({
+          uuid: `${Math.random()}`,
+          attempt: `${Math.random()}`,
+        });
+      } catch (err) {
+        expect(err.status).toBe(CaptchaNotPresentStatus);
+      }
+    });
+
+    it('Should return error from wrong attempt', async () => {
+      try {
+        const { uuid } = await appController.createChapta();
+        await appController.validateChapta({
+          uuid,
+          attempt: `${Math.random()}`,
+        });
+      } catch (err) {
+        expect(err.status).toBe(CaptchaWrongAttemptStatus);
+      }
+    });
+
+    it('Should get pass validation for chapta', async () => {
+      const chapta = await appController.createChapta();
+      const solution = await redisService.getKey(chapta.uuid);
+      let resp = false;
+      try {
+        await appController.validateChapta({
+          uuid: chapta.uuid,
+          attempt: solution,
+        });
+        resp = true;
+      } catch (err) {
+        resp = false;
+      }
+      expect(resp).toBe(true);
     });
   });
 });
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,70 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Logger,
+  Post,
+} from '@nestjs/common';
+import { CaptchaGenerationResponse } from './interfaces/captcha-generation-response.interface';
+import { CreationService } from './modules/creation/creation.service';
+import { ValidationService } from './modules/validation/validation.service';
+import { ChaptaValidationRequestDto } from './dto/chapta-validation-request.dto';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ChaptaCreateResponseDto } from './dto/chapta-create-response.dto';
+import { CaptchaNotPresentStatus } from './exceptions/CaptchaNotPresent';
+import { ChaptaDomainExceptionDto } from './dto/chapta-domain-exception.dto';
+import {
+  CaptchaWrongAttempt,
+  CaptchaWrongAttemptStatus,
+} from './exceptions/CaptchaWrongAttempt';
 
 @Controller()
+@ApiTags('chapta')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  private readonly logger = new Logger(AppController.name);
+  constructor(
+    private readonly creationService: CreationService,
+    private readonly validationService: ValidationService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('create')
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'The new chapta has been created',
+    type: ChaptaCreateResponseDto,
+  })
+  async createChapta(): Promise<CaptchaGenerationResponse> {
+    this.logger.log('Creating chapta');
+    return await this.creationService.generate();
+  }
+
+  @Post('validate')
+  @HttpCode(HttpStatus.OK)
+  @ApiBody({
+    type: ChaptaValidationRequestDto,
+    description: 'User try to decode chapta code',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description:
+      'The chapta was correct but for check user validity we must check checkValidity',
+  })
+  @ApiResponse({
+    status: CaptchaNotPresentStatus,
+    description: "The captcha that user try to validate doesn't exists anymore",
+    type: ChaptaDomainExceptionDto,
+  })
+  @ApiResponse({
+    status: CaptchaWrongAttemptStatus,
+    description: "User's attempt failed",
+    type: CaptchaWrongAttempt,
+  })
+  async validateChapta(
+    @Body() body: ChaptaValidationRequestDto,
+  ): Promise<void> {
+    this.logger.log(`Validate chapta: ${body.uuid} with: ${body.attempt}`);
+    return await this.validationService.checkValidity(body.uuid, body.attempt);
   }
 }

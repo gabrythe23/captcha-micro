0cb069b17b100c4c496a7571de25c9c26da53486
Gabriele Partiti <gabrielepartiti@icloud.com>
2022-05-26 11:37:19 +0200

    integrate static

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { CreationModule } from './modules/creation/creation.module';
 import { ValidationModule } from './modules/validation/validation.module';
 import configuration from './config/configuration';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'src/static'),
+    }),
     ConfigModule.forRoot({
       load: [configuration],
       isGlobal: true,
